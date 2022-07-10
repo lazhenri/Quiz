2f78cbe6e04cce3b0a4866d95ac756ad90382444
lazhenri <lhenriqueqoliveira@gmail.com>
2022-07-10 14:14:00 -0300

    modificando

---
+++ src/Menu.svelte
@@ -0,0 +1,20 @@
+<svelte:head>
+	<link rel="stylesheet" href="/styles/menu.css">
+</svelte:head>
+
+<script>
+	import { estado } from './Estado.js'
+	import { trocarEstadoDoJogo } from './Estado.js'
+</script>
+
+<h1>
+	QUIZ
+</h1>
+
+<div class='menu' on:click={() => trocarEstadoDoJogo('play')}>
+	PLAY
+</div>
+
+<div class='menu' on:click={() => trocarEstadoDoJogo('sobre')}>
+	SOBRE
+</div>
---
+++ src/Sobre.svelte
@@ -0,0 +1,15 @@
+<svelte:head>
+	<link rel="stylesheet" href="/styles/sobre.css">
+</svelte:head>
+
+<script>
+	import VoltarMenu from './VoltarMenu.svelte'
+</script>
+
+<h1>SOBRE</h1>
+
+
+<p>
+		jogo de perguntas, cada resposta certa soma 10 pts</p>
+
+<VoltarMenu/>
